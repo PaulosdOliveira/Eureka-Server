888e7c15d5b62beff35100d8e584efb6ebdfd622
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-03-11 20:16:44 -0300

    Criação do projeto

---
+++ eurekaserver/src/test/java/com/github/io/paulosdoliveira/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.io.paulosdoliveira.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
