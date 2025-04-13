c9970bbb56208c1f69f1629597a88efa6db1bf9e
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-04-13 02:01:44 -0300

    Aplicando a estrutura modular no projeto

--- cursor-ms/mscartoes/src/main/java/io/github/paulosdoliiveira/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ cursor-ms/mscartoes/src/main/java/io/github/paulosdoliiveira/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -1,6 +1,5 @@
 package io.github.paulosdoliiveira.mscartoes.infra.mqueue;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.paulosdoliiveira.mscartoes.infra.repository.CartaoRepository;
 import io.github.paulosdoliiveira.mscartoes.infra.repository.ClienteCartaoRepository;
